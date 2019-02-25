c1a5c33ec12fcd5f05368492ee3ff70451f50e2b
Coenraad Human <coen.human@gmail.com>
2019-02-25 15:43:06 +0200

    GUI Update

--- Java/Interface.java
+++ Java/Interface.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;  
+import javax.swing.event.*;
 
 public class Interface extends JFrame {
     JPanel[] panels = new JPanel[6];
@@ -8,24 +10,32 @@ public class Interface extends JFrame {
        2 - Speed
        3 - Active Threads
        4 - Button
-       5 - Thread*/
+       5 - Threads */
     GridBagLayout gbLayout = new GridBagLayout();
     GridBagConstraints gbc = new GridBagConstraints();
+    GridLayout gLayout = new GridLayout(0,1);
+    GridLayout gLayout2 = new GridLayout(0,4);
     JComboBox threadSelection;
     String[] threadSchemes = { "First-Come First-Serve", "Shortest Job First", "Shortest Remaining Time", "Round-Robin", "Priority Scheduling", "Multiple Queue" };
     JButton start;
     JButton stop;
+    JButton reset;
     static final int speedMin = 0;
-    static final int speedMax = 100;
-    static final int speedInit = 20;   
+    static final int speedMax = 10;
+    static final int speedInit = 8;   
     JSlider speedSlider = new JSlider(JSlider.HORIZONTAL, speedMin, speedMax, speedInit);
     static final int activeMin = 2;
     static final int activeMax = 8;
     static final int activeInit = 2; 
     JSlider activeSlider = new JSlider(JSlider.HORIZONTAL, activeMin, activeMax, activeInit);
+    MyThreads[] threads = new MyThreads[8];
+    JFrame mainWindow;
+    int speedValue = 2;
 
     public Interface() {
         super("28410629 - Thread Application");
+        mainWindow = this;
+        this.setLayout(gLayout);
         panels[0] = new JPanel();
         panels[0].setLayout(gbLayout);
         setupScheme();
@@ -33,9 +43,10 @@ public class Interface extends JFrame {
         setupActive();
         setupButton();
         setupThread();
-        this.add(panels[0]);
+        this.add(panels[0]); // main panel
+        this.add(panels[5]); // thread panel
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1024, 600);
+        this.setSize(1024, 500);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true); 
@@ -49,7 +60,7 @@ public class Interface extends JFrame {
         panels[1].setLayout(gbLayout);
         // create panel components
         threadSelection = new JComboBox(threadSchemes);
-        threadSelection.setPreferredSize(new Dimension(200,43));
+        threadSelection.setPreferredSize(new Dimension(200,86));
         // add panel components
         panels[1].add(threadSelection);
         // add panel to main panel
@@ -63,14 +74,28 @@ public class Interface extends JFrame {
         panels[2] = new JPanel();
         panels[2].setSize(420, 200);
         panels[2].setBorder(BorderFactory.createTitledBorder("Speed"));
-        panels[2].setLayout(gbLayout);
+        panels[2].setLayout(gLayout);
         // create components 
-        speedSlider.setMajorTickSpacing(20);
-        speedSlider.setMinorTickSpacing(10);
+        speedSlider.setPreferredSize(new Dimension(200, 43));
+        speedSlider.setMajorTickSpacing(1);
+        speedSlider.setMinorTickSpacing(1);
         speedSlider.setPaintTicks(true);
         speedSlider.setPaintLabels(true);
+        JLabel speedLabel = new JLabel("Set Before Starting!");
+        // add listener event
+        speedSlider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                int val = ((JSlider)e.getSource()).getValue();
+                speedLabel.setText("<html>Set Before Starting! : <font color='orange'> " + val + "</font></html>");
+                speedValue = 10 - ((JSlider)e.getSource()).getValue();
+                mainWindow.repaint();
+                mainWindow.revalidate();
+                resetThreads();
+            }
+        });
         // add panel components
         panels[2].add(speedSlider);
+        panels[2].add(speedLabel);
         // add panel to main panel
         gbc.gridx = 1;
         gbc.gridy = 0;
@@ -82,14 +107,27 @@ public class Interface extends JFrame {
         panels[3] = new JPanel();
         panels[3].setSize(420, 200);
         panels[3].setBorder(BorderFactory.createTitledBorder("Active Threads"));
-        panels[3].setLayout(gbLayout);
+        panels[3].setLayout(gLayout);
         // create components 
+        JLabel activeLabel = new JLabel("Set Before Starting!");
+        activeSlider.setPreferredSize(new Dimension(200, 43));
         activeSlider.setMajorTickSpacing(1);
         activeSlider.setMinorTickSpacing(1);
         activeSlider.setPaintTicks(true);
         activeSlider.setPaintLabels(true);
+        // add listener event
+        activeSlider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                int val = ((JSlider)e.getSource()).getValue();
+                activeLabel.setText("<html>Set Before Starting! : <font color='orange'> " + val + "</font></html>");
+                mainWindow.repaint();
+                mainWindow.revalidate();
+                resetThreads();
+            }
+        });
         // add panel components
         panels[3].add(activeSlider);
+        panels[3].add(activeLabel);
         // add panel to main panel
         gbc.gridx = 2;
         gbc.gridy = 0;
@@ -105,11 +143,41 @@ public class Interface extends JFrame {
         // create panel components
         start = new JButton("Start");
         stop = new JButton("Stop");
-        start.setPreferredSize(new Dimension(100, 43));
-        stop.setPreferredSize(new Dimension(100, 43));
+        reset = new JButton("Reset");
+        start.setPreferredSize(new Dimension(100, 86));
+        stop.setPreferredSize(new Dimension(100, 86));
+        reset.setPreferredSize(new Dimension(100, 86));
+        // button action listeners
+        start.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                if (start.getText() == "Resume") {
+                    for (int i = 0; i < activeSlider.getValue(); i++) {
+                        threads[i].resume();
+                    }                   
+                } else {
+                    for (int i = 0; i < activeSlider.getValue(); i++) {
+                        threads[i].start(); 
+                    } 
+                }
+            }  
+        });  
+        stop.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                start.setText("Resume");
+                for (int i = 0; i < activeSlider.getValue(); i++) {
+                    threads[i].suspend();
+                } 
+            }  
+        }); 
+        reset.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                resetThreads();
+            }  
+        }); 
         // add panel components
         panels[4].add(start);
         panels[4].add(stop);
+        panels[4].add(reset);
         // add panel to main panel
         gbc.gridx = 3;
         gbc.gridy = 0;
@@ -121,10 +189,37 @@ public class Interface extends JFrame {
         panels[5] = new JPanel();
         panels[5].setSize(420, 200);
         panels[5].setBorder(BorderFactory.createTitledBorder("Threads"));
-        panels[5].setLayout(gbLayout);
-        // add panel to main panel
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        panels[0].add(panels[5], gbc);
+        panels[5].setLayout(gLayout2);
+        // create components
+        for (int i = 0; i < 8; i++) {
+            threads[i] = new MyThreads(i, speedValue);
+        }
+        // add components to panel
+        for (int i = 0; i < 8; i++) {
+            panels[5].add(threads[i].getGUI());
+        }
+    }
+
+    public void resetThreads() {
+        // terminate existing threads
+        for (int i = 0; i < activeSlider.getValue(); i++) {
+            threads[i].terminate();
+            threads[i] = null;
+        } 
+        // remove components from thread panel
+        start.setText("Start");
+        panels[5].removeAll();
+        mainWindow.repaint();
+        mainWindow.revalidate();
+        // create components
+        for (int i = 0; i < 8; i++) {
+            threads[i] = new MyThreads(i, speedValue);
+        }
+        // add components to panel
+        for (int i = 0; i < 8; i++) {
+            panels[5].add(threads[i].getGUI());
+        }
+        mainWindow.repaint();
+        mainWindow.revalidate();
     }
 }
---
+++ Java/MyThreads.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyThreads extends Thread {
+
+    private JPanel panel;
+    private int threadID;
+    private JProgressBar progressBar;
+    private GridLayout gLayout = new GridLayout(0,1);
+    private GridBagLayout gbLayout = new GridBagLayout();
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private JLabel activeLabel = new JLabel("Inactive");
+    private boolean active = false;
+    private boolean finished = false;
+    private int globalSpeed;
+    private volatile boolean running = true;
+    private static final int priorityMin = 0;
+    private static final int priorityMax = 10;
+    private static final int priorityInit = 0; 
+    private JSlider prioritySlider = new JSlider(JSlider.HORIZONTAL, priorityMin, priorityMax, priorityInit);
+    private static final int workMin = 0;
+    private static final int workMax = 10;
+    private static final int workInit = 0; 
+    private JSlider workSlider = new JSlider(JSlider.HORIZONTAL, workMin, workMax, workInit);
+
+    public MyThreads(int threadID, int Speed) {
+        super();
+        createGUI(threadID);
+        globalSpeed = Speed;
+    }
+
+    public void createGUI(int id) {
+        threadID = id;
+        // create panel
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createTitledBorder("Thread : " + threadID));
+        panel.setLayout(gLayout);
+        // create components 
+        progressBar = new JProgressBar(0, 100);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+        activeLabel();
+        prioritySlider.setBackground(new Color(255,140,0));
+        workSlider.setBackground(new Color(128,128,128));
+        activeLabel.setText("<html><font color='orange'>Priority</font>, <font color='gray'>Work Intensity</font></html>");
+        // add panel components
+        panel.add(progressBar);
+        panel.add(prioritySlider);
+        panel.add(workSlider);
+        panel.add(activeLabel);
+    }
+
+    public void activeLabel() {
+        if (active) {
+            panel.setBorder(BorderFactory.createTitledBorder("<html>Thread : " + threadID + ", <font color='red'>Active</font></html>"));
+            
+        } else {
+            panel.setBorder(BorderFactory.createTitledBorder("<html>Thread : " + threadID + ", <font color='blue'>Inactive</font></html>"));
+        }
+    }
+
+    public JPanel getGUI() {
+        return panel;
+    }
+
+    public void terminate() {
+        running = false;
+    }
+
+    @Override
+    public void run(){
+        System.out.println(globalSpeed + " : " + threadID);
+        for (int i = 1; i <= 100; i++) {
+            active = true;
+            activeLabel();
+            progressBar.setValue(i);
+            try {
+                Thread.sleep(globalSpeed * 100); // global speed
+                Thread.sleep(0); // thread work intensity
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            if(i == 100) {
+                panel.setBorder(BorderFactory.createTitledBorder("<html>Thread : " + threadID + ", <font color='green'>Finished</font></html>"));
+                terminate();
+            }
+        }
+    } 
+}
